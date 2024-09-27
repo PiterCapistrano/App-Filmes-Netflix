12bf8d7862097b41a793584a95cbf53b5101c902
Piter Capistrano <pitercapistrano@gmail.com>
2024-09-27 11:11:39 -0300

    Criação das funções da Tela de cadastro

--- app/src/main/java/com/pitercapistrano/appfilmesnetflix/view/FormCadastro.kt
+++ app/src/main/java/com/pitercapistrano/appfilmesnetflix/view/FormCadastro.kt
@@ -1,7 +1,10 @@
 package com.pitercapistrano.appfilmesnetflix.view
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -26,13 +29,42 @@ class FormCadastro : AppCompatActivity() {
         window.statusBarColor = Color.parseColor("#FFFFFF")
         binding.editCdEmail.requestFocus()
 
-        binding.btCadastrar.setOnClickListener {
+        binding.btVamosLa.setOnClickListener {
             val email = binding.editCdEmail.text.toString()
 
-            if (email.isEmpty()){
+            if (!email.isEmpty()){
+                binding.containerCdSenha.visibility = View.VISIBLE
+                binding.btVamosLa.visibility = View.GONE
+                binding.txtTitulo.setText("Um mundo de séries e filmes \n ilimitados espera por você.")
+                binding.txtDescricao.setText("Crie uma conta para saber mais sobre \n o nosso app de filmes.")
+                binding.btContinuar.visibility = View.VISIBLE
+                binding.containerCdEmail.boxStrokeColor = Color.parseColor("#FF018786")
+                binding.containerCdEmail.helperText =""
+                binding.containerHeader.visibility = View.VISIBLE
+            } else{
                 binding.containerCdEmail.helperText = "O e-mail é obrigatório!"
                 binding.containerCdEmail.boxStrokeColor = Color.parseColor("#FF0000")
             }
+        }
+
+        binding.btContinuar.setOnClickListener {
+            val email = binding.editCdEmail.text.toString()
+            val senha = binding.editCdSenha.text.toString()
+
+            if (!email.isEmpty() && !senha.isEmpty()) {
+                Toast.makeText(this, "Cadastro realizado com sucesso!", Toast.LENGTH_SHORT).show()
+            } else if (senha.isEmpty()) {
+                binding.containerCdEmail.helperText = "A senha é obrigatório!"
+                binding.containerCdEmail.boxStrokeColor = Color.parseColor("#FF0000")
+                binding.containerCdEmail.boxStrokeColor = Color.parseColor("#FF018786")
+            } else if (email.isEmpty()){
+                binding.containerCdEmail.helperText = "O e-mail é obrigatório!"
+                binding.containerCdEmail.boxStrokeColor = Color.parseColor("#FF0000")
+            }
+        }
+        binding.txtEntrar.setOnClickListener {
+            val intent = Intent(this, FormLogin::class.java)
+            startActivity(intent)
         }
     }
 }
