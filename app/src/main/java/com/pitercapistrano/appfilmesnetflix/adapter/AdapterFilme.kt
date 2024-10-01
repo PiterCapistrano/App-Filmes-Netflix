087a0055c2d03c7d038283597456af188cb061eb
Piter Capistrano <pitercapistrano@gmail.com>
2024-10-01 10:48:24 -0300

    Criação da evento de click nas capas dos filmes para ir a tela de detalhes

--- app/src/main/java/com/pitercapistrano/appfilmesnetflix/adapter/AdapterFilme.kt
+++ app/src/main/java/com/pitercapistrano/appfilmesnetflix/adapter/AdapterFilme.kt
@@ -1,12 +1,14 @@
 package com.pitercapistrano.appfilmesnetflix.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.pitercapistrano.appfilmesnetflix.databinding.FilmeItemBinding
 import com.pitercapistrano.appfilmesnetflix.model.Filme
+import com.pitercapistrano.appfilmesnetflix.view.DetalhesFilme
 
 class AdapterFilme(private val context: Context, private val listaFilmes: MutableList<Filme>):
     RecyclerView.Adapter<AdapterFilme.FilmeViewHolder>() {
@@ -20,6 +22,11 @@ class AdapterFilme(private val context: Context, private val listaFilmes: Mutabl
 
     override fun onBindViewHolder(holder: FilmeViewHolder, position: Int) {
         Glide.with(context).load(listaFilmes[position].capa).centerCrop().into(holder.capa)
+
+        holder.capa.setOnClickListener {
+            val intent = Intent(context, DetalhesFilme::class.java)
+            context.startActivity(intent)
+        }
     }
 
     inner class FilmeViewHolder(binding: FilmeItemBinding): RecyclerView.ViewHolder(binding.root) {
